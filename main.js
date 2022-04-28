b6719b359c6e613f07be092657f3d947bb985e7a
Fernanda Albuquerque <fmahfouddealbuque@mail.valenciacollege.edu>
2022-04-28 17:48:18 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const fooBtn = document.getElementById("foo")
+const barBtn = document.getElementById("bar") ;
+const foobarBtn = document.getElementById("foobar") ;
+const main = document.querySelector ("main")
+
+fooBtn.addEventListener("click", function() {
+let text = document.createElement ("h3");
+text. textContent = "Foo";
+main.appendChild (text);
+});
+
+barBtn.addEventListener("click", function(){
+let text = document.createElement ("h3");
+text.textContent = "Bar";
+main.appendChild(text);
+});
+
+foobarBtn.addEventListener("click", function() {
+let text = document.createElement ("h2");
+text.textContent = "FooBar";
+main.appendChild(text);
+});
